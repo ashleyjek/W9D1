bb7ec48730350a641ffa416f33b3ce461cd3a54f
ashley <ashleykimca@gmail.com>
2023-06-26 15:31:50 -0400

    phase 2 in process

--- asteroids/src/MovingObject.js
+++ asteroids/src/MovingObject.js
@@ -9,10 +9,16 @@ export default class MovingObject {
    draw(ctx) {
     const x = this.pos[0];
     const y = this.pos[1];
-    arc(x, y, this.radius, 0, 2 * Math.PI)
     ctx.beginPath();
+    ctx.arc(x, y, this.radius, 0, 2 * Math.PI, true);
     ctx.fillStyle = `${this.color}`
     ctx.stroke();
+   }
+
+   move() {
+    const [x, y] = this.pos
+    const [i, j] = this.vel
+    this.pos = [x + i, y +j];
    }
 }
 
--- asteroids/src/index.js
+++ asteroids/src/index.js
@@ -6,5 +6,5 @@ window.MovingObject = MovingObject;
 document.addEventListener("DOMContentLoaded", () => {
     const canvas = document.getElementById("game-canvas");
     const ctx = canvas.getContext("2d");
-    
+   
 });
